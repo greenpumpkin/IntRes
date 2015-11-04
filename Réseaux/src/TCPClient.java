f1d9d6419f95f25c8f5281f6507fc6a9356bc105
Cindy Najjar <najjar@polytech.unice.fr>
2015-11-04 17:33:52 +0100

    addr

--- Réseaux/src/TCPClient.java
+++ Réseaux/src/TCPClient.java
@@ -1,4 +1,38 @@
+import java.io.*;
+import java.net.*;
 
 public class TCPClient {
+	private Socket clientSocket;
+	public static final byte[] SERVERIP = new byte[] {127, 0, 0, 1};
 
+	public TCPClient() {
+		String sentence, modifiedSentence;
+		try { 
+			InetAddress addr = Inet4Address.getByAddress(SERVERIP);
+			
+			clientSocket = new Socket(addr, 1903);
+			BufferedReader inFromUser = new BufferedReader(
+					new InputStreamReader(System.in));
+
+			DataOutputStream outToServer = new DataOutputStream(
+					clientSocket.getOutputStream());
+
+			BufferedReader inFromServer = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
+
+			sentence = inFromUser.readLine();
+			outToServer.writeBytes(sentence + '\n');
+
+			modifiedSentence = inFromServer.readLine();
+			System.out.println("FROM SERVER: " + modifiedSentence);
+			clientSocket.close();
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void main(String argv[]) throws Exception {
+		TCPClient client = new TCPClient();
+	}
 }
--- Réseaux/src/TCPServeur.java
+++ Réseaux/src/TCPServeur.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
@@ -84,21 +85,31 @@ public class TCPServeur {
 		}
 	}
 
-	public void run() throws Exception {
+	public void serveur() {
 		String reqClient;
 		String capitalizedSentence;
-		ServerSocket welcomeSocket = new ServerSocket(6789);
-
-		while (true) {
-			Socket connectionSocket = welcomeSocket.accept();
-			BufferedReader inFromClient = new BufferedReader(
-					new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(
-					connectionSocket.getOutputStream());
-			reqClient = inFromClient.readLine();
-			System.out.println("Received: " + reqClient);
-			capitalizedSentence = reqClient.toUpperCase() + '\n';
-			outToClient.writeBytes(capitalizedSentence);
+		ServerSocket welcomeSocket;
+		try {
+			welcomeSocket = new ServerSocket(1903);
+			while (true) {
+				Socket connectionSocket = welcomeSocket.accept();
+				BufferedReader inFromClient = new BufferedReader(
+						new InputStreamReader(connectionSocket.getInputStream()));
+				DataOutputStream outToClient = new DataOutputStream(
+						connectionSocket.getOutputStream());
+				reqClient = inFromClient.readLine();
+				System.out.println("Received: " + reqClient);
+				capitalizedSentence = reqClient.toUpperCase() + '\n';
+				outToClient.writeBytes(capitalizedSentence);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+
+	public static void main(String args[]) {
+		TCPServeur serv = new TCPServeur("data.txt");
+		serv.serveur();
 	}
 }
